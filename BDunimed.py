4c94b4782d17e720ab60a03dad9242bb3a2b92d1
eduardojorge7 <eduardoyoung007@gmail.com>
2022-06-26 11:06:47 -0300

    Adicao do backend e do BD

---
+++ BDunimed.py
@@ -0,0 +1,68 @@
+import random
+from pymongo import MongoClient
+
+
+uri = "mongodb+srv://Eduardo_jorge:455466@clusterhospital.kmxto.mongodb.net"
+client = MongoClient(uri)
+
+db = client['UNIMED'] # criacao do bando de dados
+
+#Variaveis paciente
+cpfPaciente = input("CPF Paciente....: ")
+nome = input("Nome Paciente....: ")
+sexo = input("Sexo....: ")
+planoUnimed = input("Plano Unimed....:")
+
+#Variaveis clinica
+cnpjClinica = input("CNPJ da clinica....: ")
+nomeClinica = input("Nome da clinica....: ")
+endereco = input("Endereco da clinica....: ")
+
+#Variaveis pedidos
+cnpjFornecedor = input("CNPJ do fornecedor....: ")
+dataSolicitacao = input("Data da solicitacao....: ")
+
+#Variaveis medicamentos
+nomeMedicamento = input("Nome do medicamento....: ")
+SLA = input("SLA....: ")
+descricao = input("Descricao do medicamento....: ")
+
+#Variaveis fornecedor
+nomeFornecedor = input("Nome do fornecedor....: ")
+
+#Variaveis consultas
+medicamentoAplicado = True
+dataConsulta = input("Data da consulta....: ")
+
+
+#insercao de toda a estrutura do banco
+db.Paciente.insert_many([{"idPaciente": random.randint(0, 9999),          #dados do paciente
+                           "cpfPaciente": cpfPaciente,
+                           "nome": nome,
+                           "sexo": sexo,
+                           "planoUnimed": planoUnimed,
+                           "Clinica": [{"idClinica": random.randint(0, 9999),   #dados da clinica
+                                        "cnpjClinica": cnpjClinica,
+                                        "nomeClinica": nomeClinica,
+                                        "endereco": endereco,
+                                        "Pedidos": [{"idPedidos": random.randint(0, 9999),    #dados dos pedidos
+                                                     "cnpjFornecedor": cnpjFornecedor,
+                                                     "cpfPaciente": cpfPaciente,
+                                                     "dataSolicitacao": dataSolicitacao,
+                                                     "Medicamentos": [{"idMedicamento": random.randint(0, 9999),    #dados dos medicamentos
+                                                                       "nomeMedicamento": nomeMedicamento,
+                                                                       "SLA": SLA,
+                                                                       "descricao": descricao
+                                                                       }],
+                                                     }],
+                                        "Fornecedor": [{"idFornecedor": random.randint(0, 9999),    #dados do fornecedor
+                                                        "nomeFornecedor": nomeFornecedor,
+                                                        "cnpjFornecedor": cnpjFornecedor
+                                                        }],
+                                        }],
+                           "Consultas": [{"idConsulta": random.randint(0, 9999),      #dados das consultas do paciente
+                                          "cnpjClinica": cnpjClinica,
+                                          "cpfPaciente": cpfPaciente,
+                                          "medicamentoAplicado": medicamentoAplicado,
+                                          "dataConsulta": dataConsulta}]
+                           }])
---
+++ schema.py
@@ -0,0 +1,112 @@
+import random
+from pymongo import MongoClient
+
+uri = "mongodb+srv://Eduardo_jorge:455466@clusterhospital.kmxto.mongodb.net"
+client = MongoClient(uri)
+
+db = client['UNIMED']
+
+#schema
+
+############### ADICIONAR NOVO PACIENTE ###############
+novoPaciente = {
+    "idPaciente": random.randint(0, 9999),
+    "cpfPaciente": 39609001922,
+    "nome": 'Eduardo Jorge',
+    "sexo": "M",
+    "planoUnimed": "Classico",
+    "Clinica": [{"idClinica": random.randint(0, 9999),
+                 "cnpjClinica": "61.412.110/0001-55",
+                 "nomeClinica": "ABC Exames",
+                 "endereco": "Rua Jasmim, 810",
+                 "Pedidos": [{"idPedidos": random.randint(0, 9999),
+                              "cnpjFornecedor": "13.840.709/0001-47",
+                              "cpfPaciente": 39609001922,
+                              "dataSolicitacao": "25/06/2000",
+                              "Medicamentos": [{"idMedicamento": random.randint(0, 9999),
+                                                "nomeMedicamento": "Dipirona, 3mg",
+                                                "SLA": 10,
+                                                "descricao": "10 comprimidos"
+                                                }],
+                              }],
+                 "Fornecedor": [{"idFornecedor": random.randint(0, 9999),
+                                 "nomeFornecedor": "XYZ fornecedores",
+                                 "cnpjFornecedor": "13.840.709/0001-47"
+                                 }],
+                 }],
+    "Consultas": [{"idConsulta": random.randint(0, 9999),
+                   "cnpjClinica": "61.412.110/0001-55",
+                   "cpfPaciente": 39609001922,
+                   "medicamentoAplicado": True,
+                   "dataConsulta": "26/06/2022"}]
+
+}
+
+db.Paciente.insert_one(novoPaciente)
+
+############### INSERIR +1 CLINICA ###############
+novaClinica = {
+    "idClinica": random.randint(0, 9999),
+    "cnpjClinica": "28.425.652/0001-97",
+    "nomeClinica": "Clinica Amor Saude",
+    "endereco": "Av. Brasil, 2066",
+    "Pedidos": [{"idPedidos": random.randint(0, 9999),
+                 "cnpjFornecedor": "13.840.709/0001-47",
+                 "cpfPaciente": 39609001922,
+                 "dataSolicitacao": "22/05/2022",
+                 "Medicamentos": [{"idMedicamento": random.randint(0, 9999),
+                                   "nomeMedicamento": "Rivotril 5mg",
+                                   "SLA": 5,
+                                   "descricao": "gotas"
+                                   }],
+                 }]
+    }
+
+
+db.Paciente.update_one(
+   { "idPaciente": 1043 },
+   { '$push': { "Clinica": novaClinica } })
+
+
+############### INSERIR +1 PEDIDO ###############
+novoPedido = {
+    "idPedidos": random.randint(0, 9999),
+     "cnpjFornecedor": "13.840.709/0001-47",
+     "cpfPaciente": 39609001922,
+     "dataSolicitacao": "22/06/2022",
+     "Medicamentos": [{"idMedicamento": random.randint(0, 9999),
+                       "nomeMedicamento": "toxina botulinica",
+                       "SLA": 5,
+                       "descricao": "frasco"
+                       }]
+    }
+
+db.Paciente.update_one(
+   { "idPaciente": 1043 },
+   { '$push': { "Pedidos": novoPedido } })
+
+
+############### INSERIR +1 FORNECEDOR ###############
+novoFornecedor = {
+    "idFornecedor": random.randint(0, 9999),
+    "nomeFornecedor": "XYZ fornecedores",
+    "cnpjFornecedor": "13.840.709/0001-47"
+}
+
+db.Paciente.update_one(
+   { "idPaciente": 1043 },
+   { '$push': { "Fornecedor": novoFornecedor } })
+
+
+############### INSERIR +1 FORNECEDOR ###############
+novaConsulta = {
+    "idConsulta": random.randint(0, 9999),
+    "cnpjClinica": "28.425.652/0001-97",
+    "cpfPaciente": 39609001922,
+    "medicamentoAplicado": "Toxina botulinica",
+    "dataConsulta": "24/06/2022"
+}
+
+db.Paciente.update_one(
+   { "idPaciente": 1043 },
+   { '$push': { "Consultas": novaConsulta } })
